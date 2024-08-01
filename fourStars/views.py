8860407710564f4aca053fa3d6e84101d4b57822
vvhornung <vvhornung@eafit.edu.co>
2024-08-01 11:13:24 -0500

    primer commit

---
+++ fourStars/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+#create your views here
+def home(request):
+    # return HttpResponse("<h1>Four Stars Project</h1>")
+    return render(request, 'home.html', {'name': 'Mavanesa'})
+
+def about(request):
+    return HttpResponse("<h1>About Four Stars </h1>")
+
