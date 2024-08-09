86e202bccba79989b809efc938e7f8046d79c516
Max-Bustamante69 <mbustamang@eafit.edu.co>
2024-08-08 21:37:08 -0500

    DATABASE LINKING FROM MAIN

--- fourStarsProject/settings.py
+++ fourStarsProject/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'fourStarsProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'fourstarsDB',
+        'USER': 'admin',
+        'PASSWORD': 'fourStarsAdmin123456',
+        'HOST': 'aws-fourstarsdb.cp440aieesph.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
+
     }
 }
 
