e84da3790d1e6b32c798a7504f49ac5bd8cdcdbd
vvhornung <vvhornung@eafit.edu.co>
2024-08-08 22:39:21 -0500

    basic model creation for mvp

--- fourStars/models.py
+++ fourStars/models.py
@@ -1,21 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
-class Student(AbstractUser):
-   # abstractuser is a built-in model that comes with django that we can use to create a user model with a username, email, first name, last name, etc.
-   #this is usefull because we can use the built-in authentication system that django provides
-   pass
-
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=50)
+    courses = models.ManyToManyField('Course', related_name='students')
+    
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+    
 class Course(models.Model):
     name = models.CharField(max_length=50)
     code = models.CharField(max_length=50)
     professors = models.ManyToManyField('Professor', related_name='courses')
 
+    def __str__(self):
+        return self.name  
+
 class Professor(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name   
     
     
  
@@ -26,3 +37,6 @@ class Rating(models.Model):
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
     rating = models.IntegerField()
     review = models.TextField()
+
+    def __str__(self):  
+        return str(self.rating) 
--- fourStarsProject/settings.py
+++ fourStarsProject/settings.py
@@ -87,8 +87,6 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = "fourStars.Student"
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
