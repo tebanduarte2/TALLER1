0ea81ff64ef7fd7f240723a69ff392d3a5aa1399
vvhornung <vvhornung@eafit.edu.co>
2024-09-01 19:59:02 -0500

    creation and linking of professor view

--- fourStars/urls.py
+++ fourStars/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('professors/', views.professors, name='professors'),
 ]
--- fourStars/views.py
+++ fourStars/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from .models import Professor
 def home(request):
     return render(request, 'fourStars/home.html')
 
 def about(request):
     return render(request, 'fourStars/about.html')
+
+def professors(request):
+    professors = Professor.objects.all()
+    return render(request, 'fourStars/professors.html', {'professors': professors})
