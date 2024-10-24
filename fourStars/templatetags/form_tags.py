177ce97d45b6d742e5cb4a43f1ce4ca9c22ea42e
Max-Bustamante69 <mbustamang@eafit.edu.co>
2024-10-24 02:27:54 -0500

    TAILWIND STYLES AND PLACEHOLDER TAG CREATED FOR FORM FIELDS

--- fourStars/templatetags/form_tags.py
+++ fourStars/templatetags/form_tags.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter(name='add_class')
 def add_class(field, css):
     return field.as_widget(attrs={"class": css})
+
+@register.filter(name='add_placeholder')
+def add_placeholder(field, placeholder_text):
+    return field.as_widget(attrs={"placeholder": placeholder_text})
